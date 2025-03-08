77133aea8621175c798ea4ca7fed050f9e69177c
aryanGajelli <argajelli@gmail.com>
2025-03-07 21:32:40 -0500

    0.5 q var

--- Lab3/ekf.py
+++ Lab3/ekf.py
@@ -18,8 +18,9 @@ ekf.F = np.array([[1, dt, 0.5*dt*dt],
                   [0, 1, dt],
                   [0, 0, 1]])
 
-ekf.Q = Q_discrete_white_noise(dim=3, dt=dt, var=3)
-ekf.R = np.diag([medium_range_var, long_range_var])*150
+ekf.Q = Q_discrete_white_noise(dim=3, dt=dt, var=0.5)
+print(ekf.Q)
+ekf.R = np.diag([medium_range_var, long_range_var])*50
 
 
 # df_exp = load_data_two_data_rows('Lab3_data/smooth25_60.mat')
@@ -44,12 +45,12 @@ medium_dist = medium_f_inv(track[:, 0])
 
 print(ekf.P)
 
-# plt.plot(time, medium_dist, alpha=.5, label='Simulated Medium')
-# plt.plot(time, long_dist, alpha=0.5, label='Simulated Long')
-# plt.plot(time, x_true, alpha=0.5, label='True Simulation x')
+plt.plot(time, medium_dist, alpha=.5, label='Simulated Medium')
+plt.plot(time, long_dist, alpha=0.5, label='Simulated Long')
+plt.plot(time, x_true, alpha=0.5, label='True Simulation x')
 plt.plot(time, xs, label=['Filter x', 'Filter x\'', 'Filter x\'\''])
-plt.plot(time, long_f_inv(df['long']), label='Measured Long')
-plt.plot(time, medium_f_inv(df['medium']), label='Measured Medium')
+# plt.plot(time, long_f_inv(df['long']), label='Measured Long')
+# plt.plot(time, medium_f_inv(df['medium']), label='Measured Medium')
 plt.grid()
 plt.legend()
 plt.xlabel('Time (s)')
